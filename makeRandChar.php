7f5529c129a9702116c4e21291187a19d1e15a8c
hjyhj <982957556@qq.com>
2018-02-02 21:03:24 +0800

    '功能生成制定类型的随机字符串'

---
+++ makeRandChar.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * 功能生成制定类型的随机字符串
+ * 预置一个的字符数组 $chars ，包括 a – z，A – Z，0 – 9，以及一些特殊字符
+ * 通过array_rand()从数组 $chars 中随机选出 $length 个元素
+ * 根据已获取的键名数组 $keys，从数组 $chars 取出字符拼接字符串。该方法的缺点是相同的字符不会重复取。
+ *
+ * @param number $length 随机选出 $length 个元素
+ * @param string $type 类型 password, char, number, lowerchar, upperchar, username
+ *
+ * @return string
+ */
+function makeRandChar( $length = 8, $type='' ) {
+    // 密码字符集，可任意添加你需要的字符
+    switch ($type){
+        case 'lowerchar':
+            $chars = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+            'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y','z');
+            break;
+        case 'upperchar':
+            $chars = array('A', 'B', 'C', 'D',
+            'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O',
+            'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z');
+            break;
+        case 'char':
+            $chars = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+            'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y','z', 'A', 'B', 'C', 'D',
+            'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O',
+            'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z');
+            break;
+        case 'password':
+            $chars = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+            'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y','z', 'A', 'B', 'C', 'D',
+            'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O',
+            'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '@','#', '$', '_');
+            break;
+        case 'charnumber':
+            $chars = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+            'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y','z', 'A', 'B', 'C', 'D',
+            'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O',
+            'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+            break;
+        case 'username':
+            $chars = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+            'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y','z', '0', '1', '2', '3',
+            '4', '5', '6', '7', '8', '9', '_');
+            break;
+        default:
+            $chars = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+            'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y','z', 'A', 'B', 'C', 'D',
+            'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O',
+            'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!',
+            '@','#', '$', '%', '^', '&', '*', '(', ')', '-', '_',
+            '[', ']', '{', '}', '<', '>', '~', '`', '+', '=', ',',
+            '.', ';', ':', '/', '?', '|');
+    }
+
+    // 在 $chars 中随机取 $length 个数组元素键名
+    $keys = array_rand($chars, $length);
+
+    $string = '';
+    for($i = 0; $i < $length; $i++) {
+        // 将 $length 个数组元素连接成字符串
+        $string .= $chars[$keys[$i]];
+    }
+
+    return $string;
+}
