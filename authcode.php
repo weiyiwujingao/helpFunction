a60efd2422099cb3763e8df3cdcf2a48bed8dd00
hjyhj <982957556@qq.com>
2018-02-02 22:55:16 +0800

    '加解密操作'

---
+++ authcode.php
@@ -0,0 +1,78 @@
+<?php
+$string = '?userid=3049221&fromuserid=2823127&key=jdi2huhu23uhu8d8y73y7y7y7';
+$operation = 'ENCODE';
+$key = 23242423430;
+$expiry = 100;
+echo $newdata = authcode($string, $operation, $key, $expiry);
+echo '<br>';
+echo authcode($newdata, 'DECODE', $key);
+
+/**
+  * 加解密操作
+  *
+  * @param string $string    加密串
+  * @param string $operation 加解密标识 DECODE:解密
+  * @param string $key       密匙
+  * @param expiry $expiry    密文有效期
+  * @return string
+  */
+function authcode($string, $operation = 'DECODE', $key = '', $expiry = 0)
+{
+
+    $ckey_length = 4;
+
+    $key = md5($key ? $key : UC_KEY);
+    $keya = md5(substr($key, 0, 16));
+    $keyb = md5(substr($key, 16, 16));
+    $keyc = $ckey_length ? ($operation == 'DECODE' ? substr($string, 0, $ckey_length): substr(md5(microtime()), -$ckey_length)) : '';
+
+    $cryptkey = $keya.md5($keya.$keyc);
+    $key_length = strlen($cryptkey);
+
+    $string = $operation == 'DECODE' ? base64_decode(substr($string, $ckey_length)) : sprintf('%010d', $expiry ? $expiry + time() : 0).substr(md5($string.$keyb), 0, 16).$string;
+    $string_length = strlen($string);
+
+    $result = '';
+    $box = range(0, 255);
+
+    $rndkey = array();
+    for($i = 0; $i <= 255; $i++)
+    {
+        $rndkey[$i] = ord($cryptkey[$i % $key_length]);
+    }
+
+    for($j = $i = 0; $i < 256; $i++)
+    {
+        $j = ($j + $box[$i] + $rndkey[$i]) % 256;
+        $tmp = $box[$i];
+        $box[$i] = $box[$j];
+        $box[$j] = $tmp;
+    }
+
+    for($a = $j = $i = 0; $i < $string_length; $i++)
+    {
+        $a = ($a + 1) % 256;
+        $j = ($j + $box[$a]) % 256;
+        $tmp = $box[$a];
+        $box[$a] = $box[$j];
+        $box[$j] = $tmp;
+        $result .= chr(ord($string[$i]) ^ ($box[($box[$a] + $box[$j]) % 256]));
+    }
+
+    if($operation == 'DECODE')
+    {
+        if((substr($result, 0, 10) == 0 || substr($result, 0, 10) - time() > 0) && substr($result, 10, 16) == substr(md5(substr($result, 26).$keyb), 0, 16))
+        {
+            return substr($result, 26);
+        }
+        else
+        {
+            return '';
+        }
+    }
+    else
+    {
+        return $keyc.str_replace('=', '', base64_encode($result));
+    }
+
+}
